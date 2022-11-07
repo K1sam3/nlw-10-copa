a5b503a64abe61f76e797a8ff76fed019ab76525
k1sam3 <herbertw_alves@hotmail.com>
2022-11-07 00:06:27 -0300

    :new: Conserto de jogos do grupo G

--- main.js
+++ main.js
@@ -25,11 +25,21 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =
-  createCard('24/11', 'quinta', createGame('brazil', '16:00', 'argentina')) +
-  createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland')) +
+  createCard(
+    '24/11',
+    'quinta',
+    createGame('switzerland', '07:00', 'cameroon') +
+      createGame('brazil', '16:00', 'serbia')
+  ) +
+  createCard(
+    '28/11',
+    'segunda',
+    createGame('cameroon', '07:00', 'serbia') +
+      createGame('brazil', '13:00', 'switzerland')
+  ) +
   createCard(
     '02/12',
     'sexta',
-    createGame('brazil', '16:00', 'cameroon') +
-      createGame('argentina', '15:00', 'brazil')
+    createGame('cameroon', '16:00', 'brazil') +
+      createGame('serbia', '16:00', 'switzerland')
   )
